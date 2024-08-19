571b17b458258fb83249aec5288fea33638e7ddf
iyo11 <IYO.MAIL.CN@gmail.com>
2024-08-20 00:24:03 +0800

    [Feat] > Test

--- JsonPad.Desktop/Program.cs
+++ JsonPad.Desktop/Program.cs
@@ -12,7 +12,7 @@ class Program
     [STAThread]
     public static void Main(string[] args) => BuildAvaloniaApp()
         .StartWithClassicDesktopLifetime(args);
-
+    
     // Avalonia configuration, don't remove; also used by visual designer.
     public static AppBuilder BuildAvaloniaApp()
         => AppBuilder.Configure<App>()
